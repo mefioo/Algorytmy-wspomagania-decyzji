7c08c88bac7df31ea0db506b8c247cad0de9e7e0
mefioo <maciek1271@gmail.com>
2021-01-13 17:18:23 +0100

    added loading file

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,52 @@
-from flask import render_template
+from flask import render_template, request, redirect, flash, url_for, make_response
+import os
+from os.path import join, dirname, realpath
 from app import app
 
 
-@app.route('/')
-@app.route('/main')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
 def main():
-    return render_template('main.html', title='Strona główna')
+    if request.method == 'GET':
+        return render_template('main.html', title='Strona główna')
+    elif request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template('main.html', title='Strona główna')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return render_template('main.html', title='Strona główna')
+        if not allowed_file(file.filename):
+            flash('Wrong file extension')
+            return render_template('main.html', title='Strona główna')
+        if file and allowed_file(file.filename):
+            filename = 'zdjecie' #secure_filename(file.filename)
+            file_path = join(dirname(realpath(__file__)), app.config['UPLOAD_FOLDER'], filename)
+            print("FILEPATH IS: "+file_path)
+            if(os.path.exists(file_path)):
+                os.remove(file_path)
+            file.save(file_path)
+            return redirect(url_for('result'))
 
 @app.route('/contact')
 def contact():
-    return render_template('Contact.html', title='Kontakt')
+    return render_template('contact.html', title='Kontakt')
 
 @app.route('/result')
 def result():
-    return render_template('result.html', title='Wynik')
+    app.convo
+    data = []
+    response = make_response(render_template('result.html', title='Wynik', data=data))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    return response
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
 
+
+UPLOAD_FOLDER = 'static/UploadedFiles'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 if __name__ == "__main__":
     app.run(debug=True)
